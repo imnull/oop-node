a846e0fcae345016b119a9322f550dc23b357ff4
Marvin <18121196@CNSUNING.COM>
2019-04-07 04:16:51 +0800

    调整输出结构

--- element/index.js
+++ element/index.js
@@ -1,12 +1,13 @@
 const Attribute = require('./attribute');
 const AttributeList = require('./attribute-list');
 const Document = require('./document');
+const ElementBase = require('./element-base');
 const ElementComment = require('./element-comment');
 const ElementText = require('./element-text');
 const Element = require('./element');
 
 module.exports = {
     Attribute, AttributeList,
-    ElementComment, ElementText, Element,
+    ElementBase, ElementComment, ElementText, Element,
     Document
 };
--- node/index.js
+++ node/index.js
@@ -1,8 +1,7 @@
 const BaseNode = require('./base-node');
 const NamedNode = require('./named-node');
 const TreeNode = require('./tree-node');
-const NamedTreeNode = require('./named-tree-node');
 
 module.exports = {
-    BaseNode, NamedNode, TreeNode, NamedTreeNode
+    BaseNode, NamedNode, TreeNode
 };
