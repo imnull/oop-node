daaa4198dee4969190921c433720426083f18be1
Marvin <18121196@CNSUNING.COM>
2019-04-07 04:20:39 +0800

    调整输出结构

--- node/index.js
+++ node/index.js
@@ -1,7 +1,6 @@
 const BaseNode = require('./base-node');
 const NamedNode = require('./named-node');
-const TreeNode = require('./tree-node');
 
 module.exports = {
-    BaseNode, NamedNode, TreeNode
+    BaseNode, NamedNode
 };
