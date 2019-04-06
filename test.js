67527b8a81a5d09e292241b6c448294187b78341
Marvin <18121196@CNSUNING.COM>
2019-04-07 04:17:05 +0800

    完善测试

--- test.js
+++ test.js
@@ -8,6 +8,8 @@ let document = new Document({
 let div = document.createElement('view');
 div.setAttribute('class', 'user-name').setAttribute('date', '2019-04-07');
 div.appendChild(document.createComment('User Name'));
+div.appendChild(document.createElement('h1')).appendChild(document.createText('Hello~'));
 div.appendChild(document.createText('Marvin'));
+div.appendChild(document.createElement('image')).setAttribute('src', 'http://asdfsadfsa.asfdfa.com');
 div.appendChild(document.createComment('User Name End'));
 console.log(div.toString());
