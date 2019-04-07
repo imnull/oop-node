095adae14d5012cadbb29b6c81967893fa72e9ee
Marvin <18121196@CNSUNING.COM>
2019-04-07 16:17:22 +0800

    优化document对象

--- element/document.js
+++ element/document.js
@@ -1,16 +1,32 @@
-const { NamedNode } = require('../node');
+const NamedNode = require('../node/named-node');
+const NodeList = require('./node-list');
+const ElementAbstract = require('./element-abstract');
+
+const Attribute = require('./attribute');
 const Element = require('./element');
 const ElementText = require('./element-text');
 const ElementComment = require('./element-comment');
-const Attribute = require('./attribute');
 
 class Document extends NamedNode {
     constructor(option){
         option = { ...option, type: 9, name: '#document' };
         super(option);
+
+        let { childNodes } = option;
+        if(childNodes instanceof NodeList){
+            childNodes = childNodes.clone();
+        } else {
+            childNodes = new NodeList({ T: ElementAbstract, parent: null, document: this });
+        }
+        this.childNodes = childNodes;
+
         let { format = false, indent = '  ' } = option;
         this.format = format;
         this.indent = indent;
+    }
+
+    appendChild(node){
+        return this.childNodes.append(node);
     }
 
     createElement(name){
@@ -28,6 +44,10 @@ class Document extends NamedNode {
 
     createAttribute(name, value){
         return new Attribute({ document: this, name, value });
+    }
+
+    toString(){
+        return this.childNodes.toString();
     }
 }
 
