9104356a69c733740e4061b5a1581c60a7cb8dbc
Marvin <18121196@CNSUNING.COM>
2019-04-10 19:56:25 +0800

    更新测试文件

--- test.js
+++ test.js
@@ -4,17 +4,24 @@ const { Document } = require('./index');
 let doc = new Document({
     format: true,   // 带格式
     indent: '    ', // 缩进字符串（默认2空格）
+    NSGlobal: 'wx',
+    NSSpliter: ':'
 });
 let div = doc.appendChild(doc.createElement('view'));
-div.setAttribute('class', 'user-name').setAttribute('date', '2019-04-07');
+div.setAttribute('class', 'user-name').setAttribute('date', '2019-04-07').setAttribute('wx:if', '2 < 1');
 div.appendChild(doc.createComment('User Name'));
 div.appendChild(doc.createElement('h1')).appendChild(doc.createText('Hello~'));
 div.appendChild(doc.createText('Marvin'));
 div.appendChild(doc.createElement('image')).setAttribute('src', 'http://asdfsadfsa.asfdfa.com');
 div.appendChild(doc.createComment('User Name End'));
-// console.log(div.toString());
-console.log(doc.toString());
+// // console.log(div.toString());
+console.log(doc.toString({
+    NSGlobal: 's',
+    NSSpliter: '-'
+}));
 
-console.log(doc.queryAll(n => n.type === 8).map(n => n.toString()).join('\n'));
+// console.log(div.attributes.list)
 
-doc.each(n => console.log(n.type, n.name), 2);
+// console.log(doc.queryAll(n => n.type === 8).map(n => n.toString()).join('\n'));
+
+// doc.each(n => console.log(n.type, n.name), 2);
