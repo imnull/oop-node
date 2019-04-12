fab7e37dc437756540d692eb0865685b551d64a4
Marvin <18121196@CNSUNING.COM>
2019-04-12 12:17:03 +0800

    完善用例

--- test.js
+++ test.js
@@ -7,17 +7,22 @@ let doc = new Document({
     NSGlobal: 'wx',
     NSSpliter: ':'
 });
-doc.appendChild(doc.createText('abcdefghijklmn~~~...!!!'));
 let div = doc.appendChild(doc.createElement('view'));
 div.setAttribute('class', 'user-name').setAttribute('date', '2019-04-07').setAttribute('wx:if', '2 < 1');
 div.appendChild(doc.createComment('User Name'));
-div.appendChild(doc.createElement('h1')).appendChild(doc.createText('Hello~'));
+let h1 = doc.createElement('h1');
+div.appendChild(h1).appendChild(doc.createText('Hello~'));
 div.appendChild(doc.createText('Marvin'));
 div.appendChild(doc.createElement('image')).setAttribute('src', 'http://asdfsadfsa.asfdfa.com');
 div.appendChild(doc.createComment('User Name End'));
+div.insertBefore(doc.createElement('yiya'), h1).appendChild(doc.createText('yiyayiya~'))
+
+doc.appendChild(doc.createText('abcdefghijklmn~~~...!!!'));
 // // console.log(div.toString());
 console.log(doc.toString({
     NSGlobal: 's',
     NSSpliter: '-',
     // elementAlone: true,
 }));
+
+console.log(doc.query(n => n.name === 'h1').query(n => n.name === '#text').path)
